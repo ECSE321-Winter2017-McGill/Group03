8d8c996003b82f060438f909009fcbe7e43ef7ad
wzs1234566 <wzs1234566@outlook.com>
2017-02-25 19:55:52 -0500

    Update StartApplication.java

--- Java/src/ca/mcgill/ecse321/TAMAS/application/StartApplication.java
+++ Java/src/ca/mcgill/ecse321/TAMAS/application/StartApplication.java
@@ -12,29 +12,6 @@ public class StartApplication {
 	public static void main(String[] args) {
 		LoginDialog login=new LoginDialog();
 		login.setVisible(true);	
-//		new MainPage().setVisible(true);
-//		final ManagementSystem rm = PersistenceXStream.initializeModelManager(fileName);
-//
-//		LoginDialog login=new LoginDialog();
-//		login.setVisible(true);
-//		login.setFocusableWindowState(true);
-//        if(login.isSucceeded()){
-////        	login.setText("Hi " + login.getUsername() + "!");
-//        	System.out.println("sss");
-//    		new PublishJobPostingPage(rm).setVisible(true);	
-//        }
-//		java.awt.EventQueue.invokeLater( new Runnable() {
-//			public void run() {
-////				new PublishJobPostingPage(rm).setVisible(true);
-//					LoginDialog login=new LoginDialog();
-//					login.setVisible(true);
-////				while(!login.isSucceeded()){
-////					
-////				}
-////				login.dispose();
-//				new PublishJobPostingPage(rm).setVisible(true);	
-//			}
-//		});
 	}
 
 }
