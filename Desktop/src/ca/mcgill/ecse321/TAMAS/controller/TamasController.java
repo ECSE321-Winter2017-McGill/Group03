2a861f222861d07039e3f7a2b2706847736cfd0b
Borui Tao <borui.tao@mail.mcgill.ca>
2017-03-26 18:10:28 -0400

    accept/reject application feature

--- Desktop/src/ca/mcgill/ecse321/TAMAS/controller/TamasController.java
+++ Desktop/src/ca/mcgill/ecse321/TAMAS/controller/TamasController.java
@@ -7,6 +7,7 @@ import ca.mcgill.ecse321.TAMAS.controller.InvalidInputException;
 import ca.mcgill.ecse321.TAMAS.model.Applicant;
 import ca.mcgill.ecse321.TAMAS.model.Application;
 import ca.mcgill.ecse321.TAMAS.model.Course;
+import ca.mcgill.ecse321.TAMAS.model.Instructor;
 import ca.mcgill.ecse321.TAMAS.model.JobPosting;
 import ca.mcgill.ecse321.TAMAS.model.ManagementSystem;
 import ca.mcgill.ecse321.TAMAS.persistence.PersistenceXStream;
@@ -129,6 +130,65 @@ public class TamasController {
 		PersistenceXStream.saveToXMLwithXStream(ms);
 		return this_applicant;
 
+	}
+	
+	public void createCourse(String aSemester, String aCourseCode, int aNumTutorial, int aNumLab, int aNumStudent, int aCredit, int aHourRequiredTa, 
+			int aHourRequiredGrader, double aBudgetCalculated, Instructor aInstructor, ManagementSystem aManagementSystem) throws InvalidInputException {
+		String error = "";
+		if (aSemester == null) {
+			error += "Please specify a semester.";
+		}
+		if (aCourseCode.equals("")) {
+			error += "Please specify the course code! ";
+		}
+		if (aInstructor.equals("")) {
+			error += "Please specify the instructor! ";
+		}
+		if (aNumTutorial < 0) {
+			error += "Must specify the number of tutorial! ";
+		}
+		if (aNumLab < 0) {
+			error += "Must specify the numebr of lab! ";
+		}
+		if (aNumStudent < 0) {
+			error += "Must specify the maximum number of students! ";
+		}
+		if (aCredit < 0) {
+			error += "Must specify the number of credits! ";
+		}
+		if (aHourRequiredTa < 0) {
+			error += "Must specify the hourly wage for TA! ";
+		}
+		if (aHourRequiredGrader < 0) {
+			error += "Must specify the hourly wage for Grader! ";
+		}
+	/*	if (aBudgetCalculated < 0) {
+			error += "Must specify the calculated budget! ";
+		}*/
+		error = error.trim();
+
+		if (error.length() > 0) {
+			throw new InvalidInputException(error);
+		}
+		ms.addCourse(new Course(aSemester, aCourseCode, aNumTutorial, aNumLab, aNumStudent,
+				aCredit, aHourRequiredTa, 
+				aHourRequiredGrader, aBudgetCalculated, 
+				aInstructor,ms));
+		PersistenceXStream.saveToXMLwithXStream(ms);
+	}
+	
+	public void acceptApplication(Application application){
+		if  (application.getApplicationStatus().equals("submitted")) {
+		application.setApplicationStatus("accpeted");
+		PersistenceXStream.saveToXMLwithXStream(ms);
+		}
+	}
+	
+	public void rejectApplication(Application application){
+		if  (application.getApplicationStatus().equals("submitted")) {
+			application.setApplicationStatus("rejected");
+			PersistenceXStream.saveToXMLwithXStream(ms);
+		}
 	}
 
 }
